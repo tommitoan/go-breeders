2f31e0871fe906c9925acbfb669758c100546905
tommitoan <htm1995@gmail.com>
2024-05-25 15:02:02 +0700

    setting up web application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 const port = ":4000"
 
-type application struct {}
+type application struct {
+	templateMap map[string]*template.Template
+	config      appConfig
+}
+
+type appConfig struct {
+	useCache bool
+}
 
 func main() {
-	//app := application{}
+	app := application{
+		templateMap: make(map[string]*template.Template),
+	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w, "Hello, world!")
-	})
+	flag.BoolVar(&app.config.useCache, "cache", false, "Use template cache")
+	flag.Parse()
+
+	srv := &http.Server{
+		Addr:              port,
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       30 * time.Second,
+		ReadHeaderTimeout: 30 * time.Second,
+		WriteTimeout:      30 * time.Second,
+	}
 
 	fmt.Println("Starting web application on port", port)
 
-	err := http.ListenAndServe(port, nil)
+	err := srv.ListenAndServe()
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
+
 }
